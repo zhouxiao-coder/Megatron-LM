0bb47d631e77b92a834f0369737ab8de610d0753
Yang Yu <reyoung@126.com>
2023-02-02 19:12:08 +0800

    Polish code

--- megatron/training.py
+++ megatron/training.py
@@ -637,7 +637,7 @@ def training_log(loss_dict, total_loss_dict, learning_rate, iteration,
         }
         if total_params != 0:
             metrics['runtime/flops'] = get_flops(total_params, elapsed_time_per_iteration)
-            print('FLOPS: {}'.format(metrics['runtime/flops']), flush=True)
+            print_rank_last('FLOPS: {}'.format(metrics['runtime/flops']), flush=True)
         wandb.log(metrics, step=iteration)
 
     if iteration % args.log_interval == 0:
@@ -702,11 +702,10 @@ def _calculate_total_params(model) -> int:
         if mpu.get_data_parallel_rank() == 0:
             params += sum([p.nelement() for p in model_module.parameters()])
 
-            print(
+            print_rank_0(
                 " > number of parameters on tensor parallel rank {}: {}".format(
                     mpu.get_tensor_model_parallel_rank(), params
-                ),
-                flush=True,
+                )
             )
     total_n_parameters = torch.tensor([params]).cuda(torch.cuda.current_device())
     torch.distributed.all_reduce(total_n_parameters)
@@ -743,31 +742,24 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
     report_memory_flag = True
     total_params = _calculate_total_params(model)
     while iteration < args.train_iters:
-        debug_print(f"1 {iteration}")
         update_num_microbatches(args.consumed_train_samples)
-        debug_print("2")
         args.curr_iteration = iteration
-        debug_print("3")
         loss_dict, skipped_iter, grad_norm, num_zeros_in_grad = \
             train_step(forward_step_func,
                        train_data_iterator,
                        model,
                        optimizer,
                        opt_param_scheduler)
-        debug_print("4")
         iteration += 1
-        debug_print("5")
         args.consumed_train_samples += mpu.get_data_parallel_world_size() * \
                                        args.micro_batch_size * \
                                        get_num_microbatches()
-        debug_print("6")
         # Logging.
         loss_scale = optimizer.get_loss_scale().item()
         params_norm = None
         if args.log_params_norm:
             params_norm = calc_params_l2_norm(model)
 
-        debug_print("7")
         report_memory_flag = training_log(loss_dict, total_loss_dict,
                                           optimizer.param_groups[0]['lr'],
                                           iteration, loss_scale,
