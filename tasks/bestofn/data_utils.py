326c5a00a4b64a8ba1ffd9e4df126b625ffbe5ff
Yang Yu <reyoung@126.com>
2023-02-14 15:39:22 +0800

    Change names

--- tasks/bestofn/data_utils.py
+++ tasks/bestofn/data_utils.py
@@ -3,11 +3,11 @@ from torch.utils.data import Dataset
 
 class BestOfNDataset(Dataset):
     def __init__(self, name: str, data_prefix: str, max_seq_length: int):
-        raise NotImplementedError("读取 process_data_best_of_n.py 生成的数据集")
+        raise NotImplementedError("读取 preprocess_data_best_of_n.py 生成的数据集")
 
     @staticmethod
     def collate_fn(batch):
-        raise NotImplementedError("""collate process_data_best_of_n.py 生成的数据集
+        raise NotImplementedError("""collate preprocess_data_best_of_n.py 生成的数据集
         
 数据集的结果返回为
   * samples:  [batch_size, N, seq_len]
